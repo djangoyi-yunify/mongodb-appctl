500b319e83350b410cda6f17dbc9519cd32fa329
Yi Jing <djangoyi@yunify.com>
2021-07-17 09:46:35 +0800

    prepare for repl init

--- ansible/roles/mongodb-replica/files/opt/app/current/bin/node/nodectl.sh
+++ ansible/roles/mongodb-replica/files/opt/app/current/bin/node/nodectl.sh
@@ -3,6 +3,15 @@
 MONGODB_DATA_PATH=/data/mongodb-data
 MONGODB_LOG_PATH=/data/mongodb-logs
 MONGODB_CONF_PATH=/data/mongodb-conf
+
+checkNodeFirstCreated() {
+  test -f $NODECTL_NODE_FIRST_CREATE
+}
+
+checkReplNeedInit() {
+  checkNodeFirstCreated && [ $ADDING_HOSTS_FLAG = "false" ]
+}
+
 # createMongodCfg
 # desc: create mongod.conf
 # file: mongod.conf, mongod-admin.conf
@@ -38,29 +47,57 @@ replication:
   replSetName: repl
 MONGOD_CONF
 
-  cat > $MONGODB_CONF_PATH/mongod-admin.conf <<MONGOD_CONF
+if checkReplNeedInit; then
+  cat > $MONGODB_CONF_PATH/mongod-repl-init.conf <<MONGOD_CONF
+systemLog:
+  destination: file
+  path: $MONGODB_LOG_PATH/mongod.log
+  logAppend: true
+  logRotate: reopen
+net:
+  port: ${CONF_MAINTAIN_NET_PORT}
+  bindIp: 0.0.0.0
+storage:
+  dbPath: $MONGODB_DATA_PATH
+  journal:
+    enabled: true
+  engine: wiredTiger
+replication:
+  oplogSizeMB: 2048
+  replSetName: repl
 MONGOD_CONF
+fi
 }
 
 checkCfgChange() {
   :
 }
 
+NODECTL_NODE_FIRST_CREATE="/data/appctl/data/nodectl.node.first.create"
+QC_PASS_FILE="/data/appctl/data/qc_pass"
 initCluster() {
+  _initCluster
+
+  # folder
   mkdir -p $MONGODB_DATA_PATH $MONGODB_LOG_PATH $MONGODB_CONF_PATH
   chown -R mongod:svc $MONGODB_DATA_PATH $MONGODB_LOG_PATH
+  # repl.key
   echo "$GLOBAL_UUID" | base64 > "$MONGODB_CONF_PATH/repl.key"
   chown mongod:svc $MONGODB_CONF_PATH/repl.key
   chmod 0400 $MONGODB_CONF_PATH/repl.key
-  _initCluster
+  # first creat flag
+  touch $NODECTL_NODE_FIRST_CREATE
+  #qc_pass
+  local encrypted=$(echo -n ${CLUSTER_ID}${GLOBAL_UUID} | sha256sum | base64)
+  echo ${encrypted:0:16} > $QC_PASS_FILE
 }
 
 initNode() {
-  createMongodConf
+  # lxc check
   _initNode
 }
 
-NET_INFO_FILE=/data/appctl/data/netinfo
+NET_INFO_FILE="/data/appctl/data/netinfo"
 saveNetInfo() {
   echo "$MY_IP:$CONF_NET_PORT" > $NET_INFO_FILE
 }
@@ -76,19 +113,25 @@ checkNetInfoChange() {
 }
 
 start() {
-  initNode
-  if [ $CHANGE_VXNET_FLAG = "true" ]; then
-    #checkNetInfoChange
-    log "change net info"
-    saveNetInfo
-  elif [ $VERTICAL_SCALING_FLAG = "true" ]; then
-    log "vertical scaling"
-  elif [ $ADDING_HOSTS_FLAG = "true" ]; then
-    log "adding hosts"
+  isNodeInitialized || initNode
+  if checkNodeFirstCreated; then
+    createMongodConf
+    if [ ! $ADDING_HOSTS_FLAG = "true" ]; then
+      log "init the replica"
+    fi
+    rm -f $NODECTL_NODE_FIRST_CREATE
   else
-    log "create new cluster"
+    if [ $CHANGE_VXNET_FLAG = "true" ]; then
+      #checkNetInfoChange
+      log "change net info"
+      #saveNetInfo
+    elif [ $VERTICAL_SCALING_FLAG = "true" ]; then
+      log "vertical scaling"
+    else
+      log "normal stop-start"
+    fi
   fi
-  _start
+  #_start
 }
 
 stop() {
